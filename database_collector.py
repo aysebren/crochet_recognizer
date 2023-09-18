32854a8acd6467ff30730b7e2783302f8e89bf16
ayse <aysebetulebren@gmail.com>
2023-09-18 14:37:45 +0200

    large image collection is added

--- database_collector.py
+++ database_collector.py
@@ -4,8 +4,8 @@ import requests
 import pandas as pd
 import os
 
-authUsername = 'eea85e442134d36c861bc2f46bafdfa0'
-authPassword = 'Ts6L8lEbVk90wej2RhVIKq1UQLjvDyKqvM1Rc_sS'
+authUsername = '' # api username
+authPassword = '' #api password
 
 class raveleryutils:
 
@@ -14,15 +14,24 @@ class raveleryutils:
         self.authPassword = authPassword
         
     def get_patterns(self, query = '', page = 1, page_size = 100, craft = 'crochet') -> pd.core.frame.DataFrame:
-        #define URL
         url = 'https://api.ravelry.com/patterns/search.json?query={}&page={}&page_size={}&craft={}'.format(query, page, page_size, craft)  
-        #make the request
         r1 = requests.get(url, auth=requests.auth.HTTPBasicAuth(self.authUsername, self.authPassword))
-        #close the connection
         r1.close()
+
         return pd.DataFrame.from_records(json.loads(r1.text)['patterns'])
     
- 
+    def get_photos(self,photo_id):
+        url = 'https://api.ravelry.com/photos/{}/sizes.json'.format(photo_id)
+
+        r1 = requests.get(url, auth=requests.auth.HTTPBasicAuth(self.authUsername, self.authPassword))
+
+        r1.close()
+        photo_list=r1.json()
+
+        for i in (photo_list['sizes']):
+            if 'large' in i.values():
+                return(i['url'])
+
 class ImageHandler():
     def __init__(self):
         pass      
@@ -32,18 +41,28 @@ class ImageHandler():
         with open(folder_name + '/' + file_name +'.jpg', 'wb') as handler:
             handler.write(img_data)
 
-raveleryutils_api = raveleryutils(authUsername, authPassword)
 
-page_size=100
-query= 'shell'
-top_hat_patterns = raveleryutils_api.get_patterns(query = query, page = 15, page_size = page_size) 
+raveleryutils_api = raveleryutils(authUsername, authPassword)
+crochet_stitch_names = ['shell' , 'popcorn' , 'crocodile', 'knit', 'jasmine', 'snowball','bobble','flurry','ripple','larksfoot','moss',
+                        'bullion','basketweave','mesh','waffle']
+
+page_size=1000
 
 img_handler_obj=ImageHandler()
-if not dir(query):
-    os.mkdir(query)
-for i in range(page_size):
-    image_url=top_hat_patterns.first_photo[i]['medium2_url']
-    img_handler_obj.save_image(query,str(i),image_url)
+
+for query in crochet_stitch_names:
+    top_hat_patterns = raveleryutils_api.get_patterns(query = query, page = 1, page_size = page_size) 
+
+    if not os.path.exists(query):
+        os.mkdir(query)
+
+    for i in range(page_size):
+        if top_hat_patterns.first_photo[i] is not None:
+            top_hat_patterns.first_photo[i]['id']
+
+            url = raveleryutils_api.get_photos(top_hat_patterns.first_photo[i]['id'])
+            if url is not None:
+                img_handler_obj.save_image(query,str(i),url)
 
 
 
